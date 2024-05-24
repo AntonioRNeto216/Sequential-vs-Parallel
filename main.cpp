4e679f94b8716e279ba71f128ca510c9b349bcb0
AntonioRNeto216 <antoniorolaneto216@gmail.com>
2024-05-23 22:30:53 -0300

    Paralelismo de dados openmp ok

--- main.cpp
+++ main.cpp
@@ -1,10 +1,105 @@
 #include <iostream>
+#include <chrono>
 
 #include <util.h>
 #include <defines.h>
 #include <parallel.h>
 #include <sequential.h>
 
+
+#include <omp.h>
+
+void performFilterOpenMP(
+    cv::Mat *input,
+    cv::Mat *result,
+    cv::Mat *filter,
+    int rows,
+    int cols
+)
+{
+    int i, j, ik, jk, mm, nn, ii, jj;
+    float pixelResult;
+
+    #pragma omp parallel private(i, j, ik, jk, mm, nn, ii, jj, pixelResult)
+    {
+        #pragma omp for collapse(2)
+        for (i = 0; i < rows; ++i)
+        {
+            for (j = 0; j < cols; ++j)
+            {
+                pixelResult = 0.0;
+
+                for (ik = 0; ik < 3; ++ik)
+                {
+                    mm = 2 - ik;
+
+                    for (jk = 0; jk < 3; ++jk)
+                    {
+                        nn = 2 - jk;
+                        
+                        ii = i + (1 - mm);
+                        jj = j + (1 - nn);
+
+                        if (ii >= 0 && ii < rows && jj >= 0 && jj < cols)
+                        {
+                            pixelResult += input->at<uchar>(ii, jj) * filter->at<float>(mm, nn);
+                        }
+
+                    }
+                }
+
+                result->at<uchar>(i, j) = cv::saturate_cast<uchar>(pixelResult);
+            }
+        }
+    }
+}
+
+void performParallelOpenMPMethod(
+    std::vector<cv::Mat> *all_video_frames,
+    std::vector<cv::Mat> *all_performed_video_frames,
+    Filters_t *filters,
+    int *numThreads
+) 
+{
+    std::cout << "Performing parallel with OpenMP method ... " << std::endl;
+
+    std::vector<cv::Mat> buffer;
+
+    omp_set_num_threads(*numThreads);
+
+    for (int i = 0; i < all_video_frames->size(); ++i)
+    {
+
+        cv::Mat h1Results = cv::Mat::zeros(
+            all_video_frames->at(i).size(), 
+            all_video_frames->at(i).type()
+        );
+
+        cv::Mat h2Results = cv::Mat::zeros(
+            all_video_frames->at(i).size(), 
+            all_video_frames->at(i).type()
+        );
+
+        performFilterOpenMP(
+            &all_video_frames->at(i),
+            &h1Results,
+            &filters->h1,
+            all_video_frames->at(i).size().height,
+            all_video_frames->at(i).size().width
+        );
+
+        performFilterOpenMP(
+            &h1Results,
+            &h2Results,
+            &filters->h2,
+            all_video_frames->at(i).size().height,
+            all_video_frames->at(i).size().width
+        );
+
+        all_performed_video_frames->at(i) = h2Results;
+
+    }
+}
 
 int main()
 {
@@ -13,9 +108,6 @@ int main()
     cv::VideoCapture videoCapture;
 
     std::vector<cv::Mat> all_video_frames;
-
-    clock_t begin_time;
-    clock_t end_time;
 
     std::string video;
     Method method;
@@ -54,7 +146,7 @@ int main()
 
     std::vector<cv::Mat> all_performed_video_frames(all_video_frames.size());
 
-    begin_time = clock();
+    auto begin_time = std::chrono::high_resolution_clock::now();
 
     switch (method)
     {
@@ -65,13 +157,13 @@ int main()
             performParallelMethod(&all_video_frames, &all_performed_video_frames, &filters, &numThreads);
             break;
         default:
-
+            performParallelOpenMPMethod(&all_video_frames, &all_performed_video_frames, &filters, &numThreads);
             break;
     }
 
-    end_time = clock();
+    auto end_time = std::chrono::high_resolution_clock::now();
 
-    std::cout << "Time to perform filters = " << (end_time - begin_time) / CLOCKS_PER_SEC << "s" << std::endl;
+    std::cout << "Time to perform filters = " << std::chrono::duration_cast<std::chrono::milliseconds>(end_time - begin_time).count() << "ms" << std::endl;
 
     playPerformedVideo(&all_performed_video_frames);
     
